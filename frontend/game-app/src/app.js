b2dbc39301d787f5455aeee6a3e0573a94011f8c
bb1 <3842623+bb1@users.noreply.github.com>
2019-05-30 13:14:57 +0200

    new logo

--- frontend/game-app/src/app.js
+++ frontend/game-app/src/app.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './app.scss';
+import logo from './tic_tac_toe.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Click to play Tic Tac Toe!
-        </p>
-        <a
+        <img src={logo} alt="Tic Tac Toe" className="App-logo"></img>
+        <h1>
+          Tic Tac Toe
+        </h1>
+        {/* <a
           className="App-link"
           href="about:blank"
           target="_blank"
           rel="noopener noreferrer"
         >
           START
-        </a>
+        </a> */}
       </header>
     </div>
   );
